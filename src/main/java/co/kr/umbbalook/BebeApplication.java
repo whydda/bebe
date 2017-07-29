4f927a5eab4dbb36c038520df2d0e0a5d4e8a460
whydda <whydda@gmail.com>
2017-07-29 09:31:18 +0900

    Initial commit

---
+++ src/main/java/co/kr/umbbalook/BebeApplication.java
@@ -0,0 +1,12 @@
+package co.kr.umbbalook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BebeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BebeApplication.class, args);
+	}
+}
